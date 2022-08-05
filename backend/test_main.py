d7f90512b521c804dc79571b7e0a0f1ee1a3aa94
Dobroslav Totev <dev@totev.de>
2022-08-05 12:20:09 +0200

    fix: remove broken test

--- backend/test_main.py
+++ backend/test_main.py
@@ -11,9 +11,3 @@ def test_read_root_fails_without_a_database_connection():
     assert response.json() != None
     assert response.json()["detail"] != None
     assert "Connection refused" in response.json()["detail"] 
-
-def test_read_root_succeeds_without_a_database_connection():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() != None
-    assert response.json()["detail"] == None
