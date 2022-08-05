dfcae22634344ee92bb9b561b33e0bc479e6deed
Dobroslav Totev <dev@totev.de>
2022-08-05 12:17:34 +0200

    chore: add failing test to test ci pipeline

--- backend/test_main.py
+++ backend/test_main.py
@@ -11,3 +11,9 @@ def test_read_root_fails_without_a_database_connection():
     assert response.json() != None
     assert response.json()["detail"] != None
     assert "Connection refused" in response.json()["detail"] 
+
+def test_read_root_succeeds_without_a_database_connection():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() != None
+    assert response.json()["detail"] == None
