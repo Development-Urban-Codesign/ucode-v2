694fc4614e3eedc51980b256e8ed79a42040a9df
KonstiDoll <konstantin.doll@urban-codesign.com>
2022-08-11 16:02:11 +0200

    get green areas in bbox and show with color

--- frontend/src/store/modules/aoi.js
+++ frontend/src/store/modules/aoi.js
@@ -7,6 +7,8 @@ const aoi = {
     state: {
         bbox : {xmin: 13.742725, ymin: 51.059803, xmax: 13.756758, ymax: 51.066950},
         overpassBuildings: null,
+        usedTagsForGreenery : {tags: ["leisure:garden", "leisure:park", "leisure:pitch" , "landuse:village_green", "landuse:grass", "landuse:garden", "landuse:cemetery", "landuse:allotments", "landuse:forest", "natural:scrub"] },
+        overpassGreenery: null,
 
     },
     mutations:{
@@ -46,6 +48,38 @@ const aoi = {
                 })
             })
         },
+        getGreeneryFromOSM({state}){
+            HTTP
+            .post('get-greenery-from-osm',{
+                bbox : state.bbox,
+                usedTagsForGreenery: state.usedTagsForGreenery
+            })
+        },
+        getGreeneryFromDB({state, rootState}){
+            HTTP
+            .get('get-greenery-from-db')
+            .then(response=>{
+                console.log(response)
+                state.overpassGreenery = new MapboxLayer({
+                    id: 'overpass_greenery',
+                    type: GeoJsonLayer,
+                    data: response.data.features,
+                    opacity: 1,
+                    stroked: false,
+                    filled: true,
+                    extruded: false,
+                    wireframe: false,
+                    getFillColor: [102, 158, 106, 255],
+                    pickable: true,
+                    
+                });
+                rootState.map.map.addLayer(state.overpassGreenery);
+                // rootState.map.map.on('click', 'overpass_buildings', (e) => {
+                //     console.log(e)
+                // })
+            })
+        },
+        
 
     },
     getters:{
