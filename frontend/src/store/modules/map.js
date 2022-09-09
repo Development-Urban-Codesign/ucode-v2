f6d772f636806bf7cd6ed61466384d99951f1af3
Torsten Holmer <43402662+TorstenHolmer@users.noreply.github.com>
2022-09-09 16:17:37 +0200

    German UI, Mainz AOI, Apple Colors, setup database for routes

--- frontend/src/store/modules/map.js
+++ frontend/src/store/modules/map.js
@@ -5,7 +5,8 @@ const map = {
         layers: [],
         sources: [],
         map: null,
-        center: { lng: 8.26952, lat: 50.0053},
+        center: { lng: 8.26952, lat: 50.0053}, // Mainz
+        //center: { lng: -73.9503, lat: 40.7835}, // Manhattan
         zoom: 15,
         minZoom: 4,
         maxZoom: 23,
