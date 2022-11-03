c779ba365945cddda200876cb3b5f4d5b4d5e030
Torsten Holmer <43402662+TorstenHolmer@users.noreply.github.com>
2022-11-03 15:16:21 +0100

    Update migration:  where is null

--- backend/db_migrations/0006.update-all-tables-with-projectIDs.sql
+++ backend/db_migrations/0006.update-all-tables-with-projectIDs.sql
@@ -1,39 +1,39 @@
 alter table comment 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update comment set project_id = '0' where project_id=null;
+update comment set project_id = '0' where project_id is null;
 
 alter table drawnline 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update drawnline set project_id = '0' where project_id=null;
+update drawnline set project_id = '0' where project_id is null;
 
 alter table building 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update building set project_id = '0' where project_id=null;
+update building set project_id = '0' where project_id is null;
 
 alter table greenery 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update greenery set project_id = '0' where project_id=null;
+update greenery set project_id = '0' where project_id is null;
 
 alter table tree 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update tree set project_id = '0' where project_id=null;
+update tree set project_id = '0' where project_id is null;
 
 alter table driving_lane 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update driving_lane set project_id = '0' where project_id=null;
+update driving_lane set project_id = '0' where project_id is null;
 
 alter table driving_lane_polygon 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update driving_lane_polygon set project_id = '0' where project_id=null;
+update driving_lane_polygon set project_id = '0' where project_id is null;
 
 alter table routes 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update routes set project_id = '0' where project_id=null;
+update routes set project_id = '0' where project_id is null;
 
 alter table traffic_signal 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update traffic_signal set project_id = '0' where project_id=null;
+update traffic_signal set project_id = '0' where project_id is null;
 
 alter table quests 
 ADD COLUMN IF NOT EXISTS project_id TEXT;
-update quests set project_id = '0' where project_id=null;
+update quests set project_id = '0' where project_id is NULL;
