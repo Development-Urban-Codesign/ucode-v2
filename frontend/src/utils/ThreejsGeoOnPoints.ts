08377e41bf90445a0dd9974c8a716a1ca20501ee
Dobroslav Totev <dev@totev.de>
2022-11-11 11:54:23 +0100

    refactor: extracted more methods

--- frontend/src/utils/ThreejsGeoOnPoints.ts
+++ frontend/src/utils/ThreejsGeoOnPoints.ts
@@ -3,6 +3,17 @@ import maplibregl, { MercatorCoordinate, type LngLatLike } from "maplibre-gl";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
+type TransformationWrapper = {
+  position: number[];
+  rotation: number;
+  scale: number;
+};
+
+type Meshes = {
+    mesh: [];
+    material: [];
+  };
+
 export const AddGeoOnPointsToThreejsScene = (
   scene: THREE.Scene,
   geoJson: any,
@@ -11,10 +22,10 @@ export const AddGeoOnPointsToThreejsScene = (
   hasRandomSize?: number[],
   hasRandomRot?: boolean
 ) => {
-  let localSceneCoordinates: [
-    { position: number[]; rotation: number; scale: number }
-  ];
-  let currentMeshes: { mesh: any; material: any } = { mesh: [], material: [] };
+  let localSceneCoordinates: [TransformationWrapper];
+
+
+  let currentMeshes: Meshes = { mesh: [], material: [] };
 
   const sceneUpdate = () => {
     console.log(scene);
@@ -33,9 +44,14 @@ export const AddGeoOnPointsToThreejsScene = (
       }) => {
         currentMeshes = { mesh: [], material: [] };
         getAllMeshes(gltf.scene);
-        localSceneCoordinates = generateTreeCoordinates(geoJson);
-        const clusters = createGeoInstances(localSceneCoordinates);
-
+        const treeCoordinates = generateTreeCoordinates(geoJson, bbox, hasRandomSize)
+        const clusters = createGeoInstances(
+          treeCoordinates,
+          currentMeshes,
+          hasRandomSize,
+          hasRandomRot
+        );
+//        debugger
         for (let index = 0; index < clusters.length; index++) {
           scene.add(clusters[index]);
         }
@@ -60,107 +76,111 @@ export const AddGeoOnPointsToThreejsScene = (
     }
   };
 
-  function generateTreeCoordinates(_geoJson: any) {
-    localSceneCoordinates = [];
-    // for (let index = 0; index < 100; index++) {
-    //   console.log(getRndNumber(0, 90))
-
-    // }
-    if (_geoJson != null) {
-      for (let index = 0; index < _geoJson.features.length; index++) {
-        const element = _geoJson.features[index].geometry.coordinates;
-
-        let rot = getRndNumber(0, Math.PI / 2);
-        let scl = getRndNumber(
-          hasRandomSize ? hasRandomSize[0] : 1,
-          hasRandomSize ? hasRandomSize[1] : 1
-        );
-        let cords = localCordsFromWorldCords(
-          maplibregl.LngLat.convert([bbox.xmin, bbox.ymin]),
-          0
-        );
-        let localPos = {
-          position: [
-            ((localCordsFromWorldCords(element, 0).x - cords.x) * 1) /
-              cords.meterInMercatorCoordinateUnits(),
-            0,
-            ((cords.y - localCordsFromWorldCords(element, 0).y) * 1) /
-              cords.meterInMercatorCoordinateUnits(),
-          ],
-          rotation: rot,
-          scale: scl,
-        }; //problematic getting position for the trees
-
-        localSceneCoordinates.push(localPos);
-      }
-    } else {
-      let _lat = 0;
-      let _long = 0;
-      for (let i = 0; i < 500; i++) {
-        _long = i * 10;
-        for (let index = 0; index < 100; index++) {
-          _lat = index * 10;
-          localSceneCoordinates.push({
-            position: [_lat, _long],
-            rotation: 0,
-            scale: 1,
-          });
-        }
-      }
-    }
-    return localSceneCoordinates;
-  }
-  const createGeoInstances = (
-    localSceneCoordinates: [
-      { position: number[]; rotation: number; scale: number }
-    ]
-  ) => {
-    const clusters = [];
-
-    for (let index = 0; index < currentMeshes.mesh.length; index++) {
-      const mesh = currentMeshes.mesh[index];
-      const material = currentMeshes.material[index];
-      var cluster = new THREE.InstancedMesh(
-        mesh,
-        material,
-        localSceneCoordinates.length, //instance count
-        false, //is it dynamic
-        false, //does it have color
-        true //uniform scale
-      );
-
-      for (let index = 0; index < localSceneCoordinates.length; index++) {
-        const matrix = new THREE.Matrix4();
-        let scale = new THREE.Vector3(1, 1, 1);
-        let rotation = new THREE.Quaternion();
-        let position = new THREE.Vector3(
-          localSceneCoordinates[index].position[2],
-          localSceneCoordinates[index].position[1],
-          localSceneCoordinates[index].position[0]
-        );
-
-        if (hasRandomSize !== undefined) {
-          scale = new THREE.Vector3(
-            localSceneCoordinates[index].scale,
-            localSceneCoordinates[index].scale,
-            localSceneCoordinates[index].scale
-          );
-        }
-        if (hasRandomRot) {
-          let rot = localSceneCoordinates[index].rotation;
-          let eulerRot = new THREE.Euler(0, rot, 0, "XYZ");
-          rotation = rotation.setFromEuler(eulerRot);
-        }
-
-        matrix.compose(position, rotation, scale);
-        cluster.setMatrixAt(index, matrix);
-      }
-      clusters.push(cluster);
-    }
-    return clusters;
-  };
   return sceneUpdate();
 };
+
+function createGeoInstances(
+  localSceneCoordinates: TransformationWrapper[],
+  currentMeshes: Meshes,
+  hasRandomSize?: number[],
+  hasRandomRot = false
+): THREE.InstancedMesh[] {
+  const clusters: THREE.InstancedMesh[] = [];
+
+  for (let index = 0; index < currentMeshes.mesh.length; index++) {
+    const mesh = currentMeshes.mesh[index];
+    const material = currentMeshes.material[index];
+    var cluster = new THREE.InstancedMesh(
+      mesh,
+      material,
+      localSceneCoordinates.length
+    );
+
+    for (let index = 0; index < localSceneCoordinates.length; index++) {
+      const matrix = new THREE.Matrix4();
+      let scale = new THREE.Vector3(1, 1, 1);
+      let rotation = new THREE.Quaternion();
+      let position = new THREE.Vector3(
+        localSceneCoordinates[index].position[2],
+        localSceneCoordinates[index].position[1],
+        localSceneCoordinates[index].position[0]
+      );
+
+      if (hasRandomSize !== undefined) {
+        scale = new THREE.Vector3(
+          localSceneCoordinates[index].scale,
+          localSceneCoordinates[index].scale,
+          localSceneCoordinates[index].scale
+        );
+      }
+      if (hasRandomRot) {
+        let rot = localSceneCoordinates[index].rotation;
+        let eulerRot = new THREE.Euler(0, rot, 0, "XYZ");
+        rotation = rotation.setFromEuler(eulerRot);
+      }
+
+      matrix.compose(position, rotation, scale);
+      cluster.setMatrixAt(index, matrix);
+    }
+    clusters.push(cluster);
+  }
+  return clusters;
+}
+
+function generateTreeCoordinates(
+  _geoJson: any,
+  bbox: BoundingBox,
+  hasRandomSize?: number[]
+): TransformationWrapper[] {
+  const localSceneCoordinates: TransformationWrapper[] = [];
+  // for (let index = 0; index < 100; index++) {
+  //   console.log(getRndNumber(0, 90))
+
+  // }
+  if (_geoJson != null) {
+    for (let index = 0; index < _geoJson.features.length; index++) {
+      const element = _geoJson.features[index].geometry.coordinates;
+
+      let rot = getRndNumber(0, Math.PI / 2);
+      let scl = getRndNumber(
+        hasRandomSize ? hasRandomSize[0] : 1,
+        hasRandomSize ? hasRandomSize[1] : 1
+      );
+      let cords = localCordsFromWorldCords(
+        maplibregl.LngLat.convert([bbox.xmin, bbox.ymin]),
+        0
+      );
+      let localPos = {
+        position: [
+          ((localCordsFromWorldCords(element, 0).x - cords.x) * 1) /
+            cords.meterInMercatorCoordinateUnits(),
+          0,
+          ((cords.y - localCordsFromWorldCords(element, 0).y) * 1) /
+            cords.meterInMercatorCoordinateUnits(),
+        ],
+        rotation: rot,
+        scale: scl,
+      }; //problematic getting position for the trees
+
+      localSceneCoordinates.push(localPos);
+    }
+  } else {
+    let _lat = 0;
+    let _long = 0;
+    for (let i = 0; i < 500; i++) {
+      _long = i * 10;
+      for (let index = 0; index < 100; index++) {
+        _lat = index * 10;
+        localSceneCoordinates.push({
+          position: [_lat, _long],
+          rotation: 0,
+          scale: 1,
+        });
+      }
+    }
+  }
+  return localSceneCoordinates;
+}
 
 function getRndNumber(min: number, max: number): number {
   return Math.random() * (max - min) + min;
