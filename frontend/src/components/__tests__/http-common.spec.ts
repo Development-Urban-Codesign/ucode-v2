f1c6a994fc6a5545d840b0a9745c09c017b696fb
Dobroslav Totev <dev@totev.de>
2022-12-16 09:50:20 +0100

    fix: removed frontend tests

--- frontend/src/components/__tests__/http-common.spec.ts
+++ frontend/src/components/__tests__/http-common.spec.ts
@@ -3,7 +3,6 @@ import { HTTP } from "@/utils/http-common"
 
 describe('HTTP Common utils test', () => {
   it('adds the default base url to the global axios object', () => {
-    console.log(HTTP)
     expect(HTTP.defaults.baseURL).toBe("http://localhost:8000")
   })
 })
